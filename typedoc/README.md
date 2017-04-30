b63ae88a2484c9913237505d49f14db48d509131
Clint Priest <hain-dev@rxv.me>
2017-04-30 08:41:05 -0500

    Part of the way there for the new typedoc based documentation.

---
+++ typedoc/README.md
@@ -0,0 +1,4 @@
+This is the main index.htm file produced by typedoc.
+
+## Markdown
+This is a markdown file
--- typedoc/gulpfile.js
+++ typedoc/gulpfile.js
@@ -1,51 +1,51 @@
 'use strict';
 
-const gulp              = require('gulp');
-const del               = require('del');
+const gulp    = require('gulp');
+const del     = require('del');
 // const replace           = require('gulp-replace');
-const typedoc           = require('gulp-typedoc');
+const typedoc = require('gulp-typedoc');
 
 gulp.task('deps', () => {
-  // return gulp.src('./hain-plugin.d.ts')
-  //   .pipe(gulp.dest('./tmp'));
-  // return gulp.src('./app/package.json')
-  //   .pipe(install({ production: true }));
+	// return gulp.src('./hain-plugin.d.ts')
+	//   .pipe(gulp.dest('./tmp'));
+	// return gulp.src('./app/package.json')
+	//   .pipe(install({ production: true }));
 });
 
 gulp.task('clean', () => {
-  return del(['./html/**']);
+	return del(['./html/**']);
 });
 
 gulp.task('typedoc', ['deps', 'clean'], () => {
-  return gulp
-    .src(["./src/*.d.ts"])
-    .pipe(typedoc({
-      mode: 'file',
-      target: 'ES2015',
+	return gulp
+		.src(["./src/*.d.ts"])
+		.pipe(typedoc({
+			mode  : 'file',
+			target: 'ES2015',
 
-      out: './html',
-      rootDir: './src',
+			out    : './html',
+			rootDir: './src',
 
-      theme: './theme',
-      name: 'hain Documentation',
+			// theme: 'default',
+			theme: './theme',
+			name : 'hain Documentation',
 
-      entryPoint: 'hain',
+			entryPoint: 'hain',
 
-      version: true,
-
-      includeDeclarations: true,
-      excludeExternals: true,
-      excludeNotExported: true,
-      hideSources: true,
-      readme: 'none',
-    }))
+			version            : true,
+			includeDeclarations: true,
+			excludeExternals   : true,
+			excludeNotExported : true,
+			hideSources        : true,
+			readme             : 'readme.md',
+		}))
 });
 
 gulp.task('watch', ['typedoc'], () => {
-  const opts = {
-    debounceDelay: 2000
-  };
-  gulp.watch(['./theme/**','./src/**'], opts, ['typedoc']);
+	const opts = {
+		debounceDelay: 2000
+	};
+	gulp.watch(['./theme/**', './src/**'], opts, ['typedoc']);
 });
 
 gulp.task('default', ['typedoc']);
